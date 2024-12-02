03545ad548d2fe842981018f9edc72fd99e8ffbe
jarlini <jalujalu998@gmail.com>
2024-12-02 15:52:37 +0530

    first commilliiilt

--- src/component/Api.js
+++ src/component/Api.js
@@ -5,7 +5,7 @@ const token = localStorage.getItem('token');
 
 // Create an Axios instance with a base URL and Authorization header
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/admin',  // Change this to your actual API base URL
+  baseURL: `${process.env.URl}/api/admin`,  // Change this to your actual API base URL
   headers: {
     Authorization: `Bearer ${token}`, // Add token to headers
   },
